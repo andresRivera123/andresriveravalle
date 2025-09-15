3f8b02105d602172e105f3cd18296fabd7b5fe5c
Andyk <carlosandresriveravalle@gmail.com>
2025-09-15 14:40:03 +0100

    New experience: Freelance web developer

--- src/assets/utils/experienceItems.jsx
+++ src/assets/utils/experienceItems.jsx
@@ -1,6 +1,14 @@
 export const EXPERIENCE = [
+    {
+    date: "January 2025 - Present",
+    title: "Freelance Web Developer - Ireland, Dublin",
+    description:
+      "Development of customized web projects and digital products for a diverse clientele. I combine creative UI/UX design with clean code to build dynamic and scalable digital experiences, from concept to deployment.",
+    linkText: "Last project",
+    link: "https://pgasesorias.cl/",
+  },
   {
-    date: "February 2024-Present",
+    date: "February 2024- January 2025",
     title: "Web designer - Ádraba",
     description:
       "Web development and support, email layout and content updating for SMEs, businesses and corporate projects. Focused on digital marketing and dealing with the client to create creative solutions. Working with tools such as HTML, CSS, JS, Wordpress, Prestashop, Figma, Photoshop.",
@@ -8,7 +16,7 @@ export const EXPERIENCE = [
     link: "https://www.adraba.com/",
   },
   {
-    date: "April 2023 - August 2023 ",
+    date: "April 2023 - August 2023",
     title: "Frontend Developer in an Electronic Invoicing System",
     description:
       "As a final thesis project, I made a point of sale web system which consumes APIs for document generation. It was developed with ReactJS, typecript, tailwindCSS, MaterialUI. I also implemented a CI/CD Continuous Integration and Delivery Pipeline for deployment on AWS. ",
